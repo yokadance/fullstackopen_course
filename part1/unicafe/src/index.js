f27878a0f6af082bd152df2201350f5c33f89a78
yokadance <michael@hazsoluciones.com.uy>
2022-03-09 23:50:11 -0300

    Ex1.6

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,17 +1,77 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom'
+import React, { useState } from 'react'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const Text = (props)=> {
+  return (  
+    <div>
+    <h1>
+      {props.value}
+    </h1>
+  </div>  
+  )
+}
+
+const Good = (props) => {
+  console.log(props)
+  return(
+    <div>
+      <button onClick={props.handleClick}> {props.text}</button>
+  </div>  
+  )
+
+}
+
+const Neutral = (props) => {
+  console.log(props)
+  return(
+    <div>
+      <button onClick={props.handleClick}> {props.text}</button>
+  </div>  
+  )
+
+}
+
+
+const Bad = (props) => {
+  console.log(props)
+  return(
+    <div>
+      <button onClick={props.handleClick}> {props.text} </button>
+     
+  </div>  
+  )
+
+}
+const Display = props => <div>{props.text} {props.value}</div>
+
+
+
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+
+
+  return (
+    <div>
+    <Text value='give feedback'/>
+    <Good handleClick = {()=> setGood(good +1)} text='good' />
+    <Neutral handleClick = {()=> setNeutral(neutral +1)} text='neutral'/>
+    <Bad handleClick = {()=> setBad(bad +1)} text='bad'/>
+    <Text value = 'statistics'/>
+    <Display text= 'good' value={good}/>
+    <Display  text= 'neutral'value={neutral}/>
+    <Display  text= 'bad' value={bad}/>
+    </div>
+  )
+}
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
