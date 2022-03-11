236b9bd30fd5dd2b1dc5345ce8e3e4eead297295
yokadance <michael@hazsoluciones.com.uy>
2022-03-11 18:15:57 -0300

    exc 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,36 +1,7 @@
 
 
 
-
-
-const Course = ({ course }) => {
-  const sum = course.parts.map(p => p.exercises).reduce((s,e) => s+e, 0)
-    //console.log('valores s y e', s, p)
-
-  return (
-    <>
-      <Header course={course.name} /> 
-      <Content parts={course.parts} />
-      <Total sum={sum} />
-    </>
-  )
-}
-
-const Header = ({ course }) => <h1>{course}</h1>
-const Content = ({ parts }) => 
-  <>
-    {parts.map(part => 
-      <Part
-        key={part.id}
-        part={part} 
-      />)}   
-  </>
- const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-  const Total = ({ sum }) => <p><strong>Number of exercises {sum}</strong></p>
-
+import Course from'./Components/Course'
 
 
 
